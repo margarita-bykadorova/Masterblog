067c961d5481c0fbb85c549a4dab8d90140f0bbb
Margarita <marharitabykadorova@gmail.com>
2025-11-16 09:00:35 +0100

    Create the index route that displays blog posts from json file

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
+
+DATA_FILE = "storage.json"
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    with open(DATA_FILE, "r", encoding="utf-8") as handle:
+        blog_posts = json.load(handle)
+    return render_template('index.html', posts=blog_posts)
 
 
 if __name__ == '__main__':
