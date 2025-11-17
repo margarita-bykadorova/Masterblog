dc053788d481a8f71ec227e05348a1b7d567c83b
Margarita <marharitabykadorova@gmail.com>
2025-11-17 04:37:26 +0100

    Add the ability to update a blog post

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ app = Flask(__name__)
 
 
 def get_data():
+    """Fetch data from the JSON file"""
     with open(DATA_FILE, "r", encoding="utf-8") as handle:
         return json.load(handle)
 
 
 def save_data(data):
+    """Update data in JSON file."""
     with open(DATA_FILE, "w", encoding="utf-8") as handle:
         json.dump(data, handle, ensure_ascii=False, indent=4)
 
@@ -19,7 +21,6 @@ def save_data(data):
 @app.route('/')
 def index():
     """Display all blog posts"""
-
     blog_posts = get_data()
     return render_template('index.html', posts=blog_posts)
 
@@ -27,7 +28,7 @@ def index():
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     """Display the form for adding a new blog post.
-    Add a new blog post to the storage file.
+    Add a new blog post to the JSON file.
     Redirect the user to the index page."""
 
     posts = get_data()
@@ -62,6 +63,38 @@ def delete(post_id):
 
     save_data(posts)
     return redirect(url_for('index'))
+
+
+@app.route('/update/<int:post_id>', methods=['GET', 'POST'])
+def update(post_id):
+    """Display the form for updating a blog post.
+    Update the title and content of the blog.
+    Redirect the user to the index page."""
+
+    posts = get_data()
+
+    post = None
+    for p in posts:
+        if p["id"] == post_id:
+            post = p
+            break
+
+    if post is None:
+        return "Post not found", 404
+
+    if request.method == 'POST':
+        author = request.form.get('author', post["author"])
+        title = request.form.get('title', post["title"])
+        content = request.form.get('content', post["content"])
+
+        post["author"] = author
+        post["title"] = title
+        post["content"] = content
+
+        save_data(posts)
+        return redirect(url_for('index'))
+
+    return render_template('update.html', post=post)
 
 
 if __name__ == '__main__':
